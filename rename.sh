25b067a8b9ecb51af42abae6120cb5dbbb6bd04b
ianmcculloch <ianmcculloch@users.noreply.github.com>
2020-04-12 17:06:54 +0200

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -1,5 +1,6 @@
-# Rename all *.vtt to *.srt
+# Delete the first two lines in every .vtt file and the append it to the .srt file
 for f in *.vtt; do
     tail -n +3 "$f" > "$f.tmp" && mv "$f.tmp" "$f"
-    mv -- "$f" "${f%.vtt}.srt"
+    cat "$f" >> "XYZ.srt"
+#   mv -- "$f" "${f%.vtt}.srt"
 done
